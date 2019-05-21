58b9ae97d3c330234af22241ddfafde531a6f8e1
MobyDrake <milkky598@yandex.ru>
2019-05-21 15:57:32 +0300

    5

--- src/lesson_5/Solution.java
+++ src/lesson_5/Solution.java
@@ -38,24 +38,36 @@ public class Solution {
         System.arraycopy(arr, 0, a1, 0, h);
         System.arraycopy(arr, h, a2, 0, h);
 
-        new Thread(new Runnable() {
+        Thread thread1 = new Thread(new Runnable() {
             @Override
             public void run() {
 
                 for (int i = 0; i < a1.length; i++) {
                     a1[i] = (float) (a1[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
-            }
-        }).start();
 
-        new Thread(new Runnable() {
+            }
+        });
+
+        Thread thread2 = new Thread(new Runnable() {
             @Override
             public void run() {
                 for (int i = 0; i < a2.length; i++) {
                     a2[i] = (float) (a2[i] * Math.sin(0.2f + i / 5) * Math.cos(0.2f + i / 5) * Math.cos(0.4f + i / 2));
                 }
+
             }
-        }).start();
+        });
+
+        thread1.start();
+        thread2.start();
+
+        try {
+            thread1.join();
+            thread2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         System.arraycopy(a1, 0, arr, 0, h);
         System.arraycopy(a2, 0, arr, h, h);
